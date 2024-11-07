9cdc3ea507de77ac1f76395f82de943994c6ce18
João Pedro Dias Caparroz <joao.diascaparroz@gmail.com>
2024-11-07 00:34:16 -0300

    feat: initiate summarization tests

---
+++ back/src/functions/ia.py
@@ -0,0 +1,40 @@
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+class RevitIa:
+    
+    def summarize(self, text: str,
+                        max_length: int = 150, 
+                        min_length: int = 30) -> str:
+
+        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+        summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
+
+        return summary
+    
+    def test_summarize(self, text: str,
+                             max_length: int = 150, 
+                             min_length: int = 30) -> str:
+        
+        tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+        model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+        
+        inputs = tokenizer(
+            text, max_length=max_length, truncation=True, return_tensors="pt"
+        )  # Limiting to 1024 tokens to fit model requirements
+
+        # Step 4: Generate summary
+        summary_ids = model.generate(
+            inputs["input_ids"],
+            max_length=max_length,
+            min_length=30,
+            length_penalty=2.0,
+            num_beams=4,
+            early_stopping=True
+        )
+
+        # Step 5: Decode the summary
+        summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+        return summary
--- docs/docs/pt/index.md
+++ docs/docs/pt/index.md
@@ -3,3 +3,7 @@
 <h6>The loopdevs document ai</h6>
 
 ---
+
+
+#####
+    https://huggingface.co/facebook/bart-large-cnn
